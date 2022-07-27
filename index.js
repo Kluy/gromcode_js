27ab8b3185626d198f25d68404719349485f91dc
Kluy <kluy@i.ua>
2022-07-27 20:12:56 +0300

    l13t3c2

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // export default arr =>
 
-const sss = arr =>{
-    if(!Array.isArray(arr) || arr.length === 0)
-    return null;
-    
-return Math.pow(Math.min(...arr.map(elem => Math.abs(elem))), 2);
-}
+const sss = arr => Math.pow(Math.min(...arr.map(elem => Math.abs(elem))), 2);
 
-console.log(sss([-777, 3/2, -2/3, 6, 45, -20]));
+
+   
+console.log(sss([-777, 3, -2, 6, 45, -20]));
--- lesson13/task3/getMinSquareNumber.test.js
+++ lesson13/task3/getMinSquareNumber.test.js
@@ -9,5 +9,5 @@ it('should get null if parameter is a string', () => {
 })
 
 it('should get modulo min number', () => {
-    expect(getMinSquareNumbers('text')).toEqual(null);
+    expect(getMinSquareNumbers([-777, 3, -2, 6, 45, -20])).toEqual(4);
 })
