f4667fac54533a2623a5d2f623b8efadd5fc0898
Kluy <kluy@i.ua>
2022-07-27 20:13:30 +0300

    l13t3c3

--- lesson13/task3/getMinSquareNumber.test.js
+++ lesson13/task3/getMinSquareNumber.test.js
@@ -8,6 +8,6 @@ it('should get null if parameter is a string', () => {
     expect(getMinSquareNumbers('text')).toEqual(null);
 })
 
-it('should get modulo min number', () => {
+it('should get min modulo number', () => {
     expect(getMinSquareNumbers([-777, 3, -2, 6, 45, -20])).toEqual(4);
 })
