927daf43af6f9cb77bdc7b24fed2f19503a120d2
Kluy <kluy@i.ua>
2022-08-11 15:37:31 +0300

    l21t4c1

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const timer = {
-  secondsPassed,
-  minsPassed,
-  startTimer(){
-    setInterval
-  }
-}
-
-const defer = (func, ms) => {
-  return function(){
-    setTimeout(() => func.apply(this, arguments), ms);
-  }
-}
-
-
-
-const f = () => {
-  console.log(12);
-}
-
-
-const arr = defer(f,1000);
-
-arr();
+export const timer = {
+  // put your code here
+  secondsPassed: 0,
+  minsPassed: 0,
+  startTimer() {
+    setInterval(() => {
+      this.secondsPassed++;
+      if (this.secondsPassed === 60) {
+        this.minsPassed++;
+        this.secondsPassed = 0;
+      }
+    }, 1000);
+  },
+  getTime() {
+    return this.secondsPassed <= 9
+      ? `${this.minsPassed}:0${this.secondsPassed}`
+      : `${this.minsPassed}:${this.secondsPassed}`;
+  },
+  stopTimer(timerID) {
+    clearInterval(timerID);
+  },
+  resetInterval() {
+    this.secondsPassed = 0;
+    this.minsPassed = 0;
+  },
+};
---
+++ lesson21/t4/index.js
@@ -0,0 +1,7 @@
+export const getTitle = () => document.querySelector('.title').textContent;
+
+export const getDescription = () => document.querySelector('.about').innerText;
+
+export const getPlans = () => document.querySelector('.plans').innerHTML;
+
+export const getGoal = () => document.querySelector('.goal').outerHTML;
