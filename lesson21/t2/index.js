8a297e9627e478eef42261ee862bd564c1a884c5
Kluy <kluy@i.ua>
2022-08-11 14:03:41 +0300

    l21t2c2

--- lesson21/t2/index.js
+++ lesson21/t2/index.js
@@ -3,9 +3,11 @@
 export const getTitleElement = () => {
     const titleElem = document.querySelector('.title');
     console.dir(titleElem);
+    return titleElem;
 }
 
 export const getInputElem = () => {
   const inputElem = document.querySelector('input[type=text]');
   console.dir(inputElem);
+  return inputElem;
 }
