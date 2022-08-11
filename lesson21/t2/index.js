d823e21c701acc2c344b6d39b13307af819c7f97
Kluy <kluy@i.ua>
2022-08-11 14:02:36 +0300

    l21t2c1

---
+++ lesson21/t2/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+}
+
+export const getInputElem = () => {
+  const inputElem = document.querySelector('input[type=text]');
+  console.dir(inputElem);
+}
