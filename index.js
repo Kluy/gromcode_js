4cf819a30a87c8e2d027864dacd55cead8eefec2
Kluy <kluy@i.ua>
2022-08-12 09:25:22 +0300

    l21t7c1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-export const timer = {
+const timer = {
   // put your code here
   secondsPassed: 0,
   minsPassed: 0,
+  timerID: 0,
   startTimer() {
-    setInterval(() => {
+    this.timerID = setInterval(() => {
       this.secondsPassed++;
       if (this.secondsPassed === 60) {
         this.minsPassed++;
@@ -16,8 +17,8 @@ export const timer = {
       ? `${this.minsPassed}:0${this.secondsPassed}`
       : `${this.minsPassed}:${this.secondsPassed}`;
   },
-  stopTimer(timerID) {
-    clearInterval(timerID);
+  stopTimer() {
+    clearInterval(this.timerID);
   },
   resetInterval() {
     this.secondsPassed = 0;
---
+++ lesson21/t7/index.js
@@ -0,0 +1,3 @@
+export const clearList = () => {
+    document.querySelector('.categories').innerHTML = '';
+}
