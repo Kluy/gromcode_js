23ca728f0cb8abfcacb356a6686c8938d01cb396
Kluy <kluy@i.ua>
2022-09-03 22:40:56 +0300

    l24t2c2

--- lesson24/t2/index.js
+++ lesson24/t2/index.js
@@ -1,5 +1,9 @@
 export const getGreenwichTime = (date) => {
-  const UTCHours = date.getUTCHours(); 
-  const zero = UTCHours < 10 ? 0 : '';
-  return  `${zero}${UTCHours}:${date.getUTCMinutes()}`; 
+  const formatter = new Intl.DateTimeFormat('en',{
+    timeZone: 'UTC',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  })
+  return formatter.format(date); 
 }
