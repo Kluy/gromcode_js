23462e95ba60acb0a926459ffae11038032c0545
Kluy <kluy@i.ua>
2022-09-05 13:33:45 +0300

    l24t3c3

--- lesson24/t3/index.js
+++ lesson24/t3/index.js
@@ -1,8 +1,11 @@
 export const getDiff = (startDate, endDate) => {
   const millisecTotal = Math.abs(startDate - endDate);
-  const days = Math.trunc(millisecTotal / 86400000);
-  const hours = Math.abs(startDate.getHours() - endDate.getHours());
-  const mins = Math.abs(startDate.getMinutes() - endDate.getMinutes());
-  const sec = Math.abs(startDate.getSeconds() - endDate.getSeconds());
+  const secTotal = Math.trunc(millisecTotal / 1000);
+  const sec = secTotal % 60;
+  const minsTotal = (secTotal - sec) / 60;
+  const mins = minsTotal % 60;
+  const hoursTotal = (minsTotal - mins) / 60;
+  const hours = hoursTotal % 24;
+  const days = (hoursTotal - hours) / 24;
   return `${days}d ${hours}h ${mins}m ${sec}s`;
 }
