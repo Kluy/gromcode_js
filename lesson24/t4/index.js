2d5e12ba2901537a16fcab7261cdcd53892cc2af
Kluy <kluy@i.ua>
2022-09-05 16:19:02 +0300

    l24t4c1

--- lesson24/t4/index.js
+++ lesson24/t4/index.js
@@ -1,14 +1,14 @@
 // export
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+const dates = {};
+
  const studentBirthDays = (students) => {
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-  const dates = {};
-  const std = students.map(elem => {
+  const studentsArray = students.map(elem => {
     const birthDateSplit = elem.birthDate.split('/');
     const date = new Date(birthDateSplit[2], birthDateSplit[0] - 1, birthDateSplit[1]);
     const month = months[date.getMonth()];
     dates[month] = [];
     dates[month].push(elem.name);
-    console.log(date);
     console.log(month);
     console.log(dates);
   })
