06dcfaaa55c1907beb8a728b10d40349f45cfdb6
Kluy <kluy@i.ua>
2022-09-06 13:08:42 +0300

    l24t4c1

--- lesson24/t4/index.js
+++ lesson24/t4/index.js
@@ -1,24 +1,17 @@
-// export
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-const dates = {};
-
- const studentBirthDays = (students) => {
-  const studentsArray = students.map(elem => {
+export const studentBirthDays = (students) => {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  const sortedBirthDays = {};
+  const sortedStudents = students.map(elem => {
     const birthDateSplit = elem.birthDate.split('/');
-    const date = new Date(birthDateSplit[2], birthDateSplit[0] - 1, birthDateSplit[1]);
-    const month = months[date.getMonth()];
-    dates[month] = [];
-    dates[month].push(elem.name);
-    console.log(month);
-    console.log(dates);
-  })
-
-  // return `${days}d ${hours}h ${mins}m ${sec}s`;
+    elem.birthDate = new Date(birthDateSplit[2], birthDateSplit[0] - 1, birthDateSplit[1]);
+    return elem;
+  }).sort((a, b) => {
+    return a.birthDate.getDate() > b.birthDate.getDate();
+  }).map(elem => {
+    const month = months[elem.birthDate.getMonth()];
+    if(sortedBirthDays[month] === undefined){
+      sortedBirthDays[month] = [];
+    }
+    sortedBirthDays[month].push(elem.name); 
+  });
 }
-
-
-studentBirthDays([
-  {name: 'Tom', birthDate: '01/15/2010'},
-  {name: 'Ben', birthDate: '01/17/2008'},
-  {name: 'Sam', birthDate: '03/15/2010'},
-]);
