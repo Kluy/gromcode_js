8667253709f597abe1fbd56f1f2eb23a77463c04
Kluy <kluy@i.ua>
2022-09-06 13:58:35 +0300

    l24t4c6

--- lesson24/t4/index.js
+++ lesson24/t4/index.js
@@ -1,14 +1,10 @@
 export const studentsBirthDays = (students) => {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   const sortedBirthDays = {};
-  const sortedStudents = students.map(elem => {
-    const birthDateSplit = elem.birthDate.split('/');
-    elem.birthDate = new Date(birthDateSplit[2], birthDateSplit[0] - 1, birthDateSplit[1]);
-    return elem;
-  }).sort((a, b) => {
-    return a.birthDate.getDate() > b.birthDate.getDate();
+  const sortedStudents = students.sort((a, b) => {
+    return a.birthDate.split('/')[1] > b.birthDate.split('/')[1];
   }).map(elem => {
-    const month = months[elem.birthDate.getMonth()];
+    const month = months[(Number)(elem.birthDate.split('/')[0])];
     if(sortedBirthDays[month] === undefined){
       sortedBirthDays[month] = [];
     }
