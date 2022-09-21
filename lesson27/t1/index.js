ea50850471a42576445abd49700b03164dd4e876
Kluy <kluy@i.ua>
2022-09-21 23:50:51 +0300

    21092022c2

--- lesson27/t1/index.js
+++ lesson27/t1/index.js
@@ -1,8 +1,15 @@
 export const getLocalStorageData = () => {
     return Object.entries(localStorage)
-    .reduce((acc,[key,value]) => {
+    .reduce((acc, [key, value]) => {
+      let newValue;
+      try{
+        newValue = JSON.parse(value);
+      }
+      catch(e){
+        newValue = value;
+      }
       return {
         ...acc,
-         [key] : value
-        };
-          }, {})};
+         [key] : newValue
+      };
+    },{})};
