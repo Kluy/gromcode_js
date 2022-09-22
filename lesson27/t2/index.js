b1896124be32b970e279da078f1ac1041822de59
Kluy <kluy@i.ua>
2022-09-22 13:53:48 +0300

    l27t2c1

---
+++ lesson27/t2/index.js
@@ -0,0 +1,33 @@
+const counter = document.querySelector('.counter');
+const counterValueElement = document.querySelector('.counter__value');
+
+const onCounterChange = event => {
+    if(!event.target.classList.contains('counter__button'))
+    return;
+
+    const action = event.target.dataset.action;
+    console.log(action);
+
+    const previousValue = Number(counterValueElement.textContent);
+
+    const newValue = action === 'decrease' ?  previousValue - 1 : previousValue + 1;
+    
+    counterValueElement.textContent = newValue;
+
+    localStorage.setItem('counter', newValue);
+}
+
+counter.addEventListener('click', onCounterChange);
+
+const onStorageChange = event => {
+    counterValueElement.textContent = event.newValue;
+}
+
+window.addEventListener('storage', onStorageChange);
+
+const onDocumentLoaded = () => {
+    counterValueElement.textContent = localStorage.getItem('counter');
+
+}
+
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
