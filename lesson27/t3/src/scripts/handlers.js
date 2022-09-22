ebd7bc938d17db413c691c4d7e8c9aa7738ee040
Kluy <kluy@i.ua>
2022-09-23 00:01:20 +0300

    l27t3c7

--- lesson27/t3/src/scripts/handlers.js
+++ lesson27/t3/src/scripts/handlers.js
@@ -1,9 +1,9 @@
 import { addNewTask } from "./addTask.js";
 import { updateTask } from "./updateTask.js";
 
-export const listElem = document.querySelector('.list');
 
 export const initHandlers = () => {
+    const listElem = document.querySelector('.list');
     listElem.addEventListener('click', updateTask);
 
     const createNewTaskButton = document.querySelector('.create-new-task-btn');
