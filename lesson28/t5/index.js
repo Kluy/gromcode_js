062a786591eef27cf7444ee0237a6e4691df468d
Kluy <kluy@i.ua>
2022-09-23 16:47:39 +0300

    l28t5c1

---
+++ lesson28/t5/index.js
@@ -0,0 +1,39 @@
+const getMethodsNames = {
+    years: 'getFullYear',
+    months: 'getMonth',
+    days: 'getDate',
+    hours: 'getHours',
+    minutes: 'getMinutes',
+    seconds: 'getSeconds',
+    milliseconds: 'getMilliseconds',
+  };
+  
+  const setMethodsNames = {
+    years: 'setFullYear',
+    months: 'setMonth',
+    days: 'setDate',
+    hours: 'setHours',
+    minutes: 'setMinutes',
+    seconds: 'setSeconds',
+    milliseconds: 'setMilliseconds',
+  };
+
+ const shmoment = intiValue => {
+    let result = intiValue;
+    const calculator = {
+      add(unit, value){
+        if(unit === 'years'){
+           const newValue = result[getMethodsNames[unit]]() + value;
+           result[setMethodsNames[unit]](newValue);
+        }
+        return this;
+      },
+      subtract(unit, value){
+        return this.add(unit, -value);
+      },
+      result(){
+        return new Date(result);
+      }
+    };
+    return calculator;
+  }
