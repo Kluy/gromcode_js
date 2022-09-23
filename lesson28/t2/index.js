823fa7ab1b962a19cf82a44f9669d118c0b4ca26
Kluy <kluy@i.ua>
2022-09-23 14:21:43 +0300

    l28t2c2

--- lesson28/t2/index.js
+++ lesson28/t2/index.js
@@ -1,37 +1,4 @@
-// export
- 
-
-const favorites = ['id-2', 'id-4'];
-
-const tree = {
-  id: 'id-1',
-  names: 'a',
-  nodes : [
-    {
-      id: 'id-3',
-      names: 'Food',
-      nodes: [
-        {
-          id: 'id-8',
-          names: 'Drink',
-          nodes: [] 
-        },
-        {
-          id: 'id-4',
-          names: 'Carrot',
-          nodes: []
-        }
-      ]  
-    },
-    {
-      id: 'id-7',
-      names: 'Water',
-      nodes: []
-    }
-  ]
-}
-
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
   return {
     ...tree,
@@ -39,5 +6,3 @@ const markFavorites = (tree, favorites) => {
     nodes: tree.nodes.map(innerNode => markFavorites(innerNode, favorites))
   }
 }
-
-console.log(markFavorites(tree, favorites));
