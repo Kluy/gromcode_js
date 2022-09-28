6b68f129f034cba94081bc3113d57edf1abb1776
Kluy <kluy@i.ua>
2022-09-28 16:09:49 +0300

    l31t2c2

--- lesson31/t2/index.js
+++ lesson31/t2/index.js
@@ -2,13 +2,36 @@
 
 export const asyncCalculator = numbr => new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log(`Initial value: ${numbr}`);
     resolve(numbr);
   }, 500)
-}).then(data => new Promise((resolve, reject) => {
+});
+
+asyncCalculator(5).then(data => {
+  console.log(`Initial value: ${data}`);
+ return new Promise((resolve) => {
   setTimeout(() => {
     const result = data * data;
-    console.log(`Squared value: ${result}`);
     resolve(result);
   }, 500)
-})).then(data => console.log(`Doubled value: ${data * 2}`));
+})}).then(data => {
+  console.log(`Squared value: ${data}`);
+  console.log(`Doubled value: ${data * 2}`)
+})
+
+
+
+
+// 'use strict';
+
+// export const asyncCalculator = numbr => new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log(`Initial value: ${numbr}`);
+//     resolve(numbr);
+//   }, 500)
+// }).then(data => new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const result = data * data;
+//     console.log(`Squared value: ${result}`);
+//     resolve(result);
+//   }, 500)
+// })).then(data => console.log(`Doubled value: ${data * 2}`));
