568f3cc6d0604b6d73079d2830cca39118b2d474
Kluy <kluy@i.ua>
2022-09-28 13:37:48 +0300

    l29t4c2

--- lesson29/t4/index.js
+++ lesson29/t4/index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const tom = {
+  name: 'tom'
+}
+
+const bob = {
+  name: 'bob'
+}
+
+function funcUser () {
+  console.log(`${[...arguments]} ${this.name}`);
+}
+
 export const delay = (delay, callback, context, ...args) => {
   setTimeout(() => callback.bind(context)(args), delay);
 }
+
+delay(2000, funcUser, bob, 4, 5, 33);
