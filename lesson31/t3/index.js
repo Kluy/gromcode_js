1c1a4c0932c62564869b51a9356e4fe1c18495d5
Kluy <kluy@i.ua>
2022-09-28 15:47:37 +0300

    l31t3c1

---
+++ lesson31/t3/index.js
@@ -0,0 +1,37 @@
+const successPromise = new Promise(resolve => {
+  resolve(32);
+});
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+  .then(number => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then(number => {
+    /* fix this handler */
+    const squaredNumber = number * number;
+    return squaredNumber;
+  })
+  .then(result => {
+    console.log(result); // 256
+  });
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+  .then(number => {
+    /* fix this handler */
+    return number * 10;
+  })
+  .then(result => {
+    console.log(result); // 320
+  });
+
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
