928d0de4457e758b6194e2e665bd24763248f076
Kluy <kluy@i.ua>
2022-09-28 13:12:27 +0300

    l29t5c3

--- lesson29/t5/index.js
+++ lesson29/t5/index.js
@@ -8,24 +8,18 @@
 
 const time = () => (Math.round(Math.random() * 2 + 1)) * 1000;
 
-// const f = (user) => {
-// console.log(user);
-// }
-// export
+const func = (user, error) => {
+  user === null ? console.log(error) : console.log(user);
+}
 export const requestUserData = (userId, callback) => {
   // put your code here
-  
-  const user = {
-    userId,
-    email: `${userId}@example.com`
-  }
-  
-  
-  setTimeout(() => {
-    userId === 'broken' ? callback(null, 'Faild to load user data') : callback(user) 
-  }, time());
-  
+
+  userId === 'broken' ?
+    callback(null, new Error('Failed to load user data')) : setTimeout(() => {
+    callback({userId, email: `${userId}@example.com`
+  })
+}, time());  
 };
 
-// requestUserData(222, f);
-// requestUserData('broken', f);
+requestUserData(222, func);
+requestUserData('broken', func);
