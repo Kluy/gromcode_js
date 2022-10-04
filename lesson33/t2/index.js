8ec70356b44c71dfaaa0722c0c61bcbca3274af8
Kluy <kluy@i.ua>
2022-10-04 15:40:49 +0300

    l33t2c1

---
+++ lesson33/t2/index.js
@@ -0,0 +1,42 @@
+
+
+
+const getStartDate = (days) => new Date().setDate(new Date().getDate() - days);
+
+export const getMostActiveDevs = ({days, userId, repoId}) => 
+  fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+    .then(response => response.json())
+    .then(result => result.filter(elem => new Date(elem.commit.committer.date) > getStartDate(days)))
+    .then(result => {
+      const user = {}
+
+      if(result.length > 0) {
+        user.count = result.length;
+        user.name = `${result[0].commit.author.name}`;
+        user.email = `${result[0].commit.author.email}`;
+      }
+
+      return user;
+});
+
+const developer = {
+  days: 7,
+  userId: 'Kluy',
+  repoId: 'gromcode_js'
+}
+
+const developer2 = {
+  days: 7,
+  userId: 'alvarobartt',
+  repoId: 'investiny'
+}
+
+const developer3 = {
+  days: 7,
+  userId: 'coder',
+  repoId: 'coder'
+}
+
+
+Promise.all([getMostActiveDevs(developer),getMostActiveDevs(developer2),getMostActiveDevs(developer3)])
+.then(result => result.sort((a, b) => b.count - a.count).filter(elem => elem.count === result[0].count));
