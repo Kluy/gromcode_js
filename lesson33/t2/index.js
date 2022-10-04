2203d6dbaa0f900f0abee1423dbc0fa7ef1b0956
Kluy <kluy@i.ua>
2022-10-04 16:18:53 +0300

    l33t2c6

--- lesson33/t2/index.js
+++ lesson33/t2/index.js
@@ -1,7 +1,8 @@
 const getStartDate = (days) => new Date().setDate(new Date().getDate() - days);
 
-export const getMostActiveDevs = ({days, userId, repoId}) => 
-  fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+// export
+const sendRequest = (days, userId, repoId) => {
+const pr = fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
   .then(response => response.json())
   .then(result => {
     const resultArr = result.filter(elem => new Date(elem.commit.committer.date) > getStartDate(days));
@@ -15,6 +16,36 @@ export const getMostActiveDevs = ({days, userId, repoId}) =>
     
     return user;
   })
+  return pr;
+}
+
+const getMostActiveDevs = ({days, userId, repoId}) => {
+
+  
+
+  return Promise.all(sendRequest(days, userId,repoId));
+}
+
+// const getMostActiveDevs = ({days, userId, repoId}) => {
+
+//   const pr = fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+//   .then(response => response.json())
+//   .then(result => {
+//     const resultArr = result.filter(elem => new Date(elem.commit.committer.date) > getStartDate(days));
+//     const user = {}
+    
+//     if(resultArr.length > 0) {
+//       user.count = resultArr.length;
+//       user.name = `${resultArr[0].commit.author.name}`;
+//       user.email = `${resultArr[0].commit.author.email}`;
+//     }
+    
+//     return user;
+//   })
+
+//   return Promise.all(pr);
+// }
+
 
   
 const developer = {
@@ -38,5 +69,7 @@ getMostActiveDevs(developer);
 getMostActiveDevs(developer2);
 getMostActiveDevs(developer3);
 
-Promise.all([getMostActiveDevs(developer),getMostActiveDevs(developer2),getMostActiveDevs(developer3)])
-.then(result => result.sort((a, b) => b.count - a.count).filter(elem => elem.count === result[0].count));
+// console.log(getMostActiveDevs(developer3));
+
+// Promise.all([getMostActiveDevs(developer),getMostActiveDevs(developer2),getMostActiveDevs(developer3)])
+// .then(result => result.sort((a, b) => b.count - a.count).filter(elem => elem.count === result[0].count));
