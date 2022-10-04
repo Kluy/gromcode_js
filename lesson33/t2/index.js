9b375c3ab502aa7ff226d7ebc4531f242d5523e6
Kluy <kluy@i.ua>
2022-10-04 15:47:18 +0300

    l33t2c3

--- lesson33/t2/index.js
+++ lesson33/t2/index.js
@@ -1,21 +1,24 @@
 const getStartDate = (days) => new Date().setDate(new Date().getDate() - days);
 
-export const getMostActiveDevs = ({days, userId, repoId}) => 
-  fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
-    .then(response => response.json())
-    .then(result => result.filter(elem => new Date(elem.commit.committer.date) > getStartDate(days)))
-    .then(result => {
-      const user = {}
+// export
+ const getMostActiveDevs = ({days, userId, repoId}) => {
 
-      if(result.length > 0) {
-        user.count = result.length;
-        user.name = `${result[0].commit.author.name}`;
-        user.email = `${result[0].commit.author.email}`;
-      }
-
-      return user;
-});
-
+ return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+  .then(response => response.json())
+  .then(result => result.filter(elem => new Date(elem.commit.committer.date) > getStartDate(days)))
+  .then(result => {
+    const user = {}
+    
+    if(result.length > 0) {
+      user.count = result.length;
+      user.name = `${result[0].commit.author.name}`;
+      user.email = `${result[0].commit.author.email}`;
+    }
+    
+    return user;
+  });
+}
+  
 const developer = {
   days: 7,
   userId: 'Kluy',
