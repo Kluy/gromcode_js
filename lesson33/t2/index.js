f69761ef7fa307484c4a6d1a6f8e7d1284cc3b2e
Kluy <kluy@i.ua>
2022-10-06 14:35:40 +0300

    l33t2c17

--- lesson33/t2/index.js
+++ lesson33/t2/index.js
@@ -11,24 +11,29 @@ const getStartDate = (days) => new Date().setDate(new Date().getDate() - days);
   fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
   .then(response => response.json())
   .then(result => {
-    const obj = {};
-    result
+    const obj = result
     .filter(elem => new Date(elem.commit.author.date) > getStartDate(days))
-    .map(elem => {
+    .reduce((acc, elem) => {
       const authorName = elem.commit.author.name;
-      if(obj[authorName] === undefined){
-        obj[authorName] = {
+      return {
+        ...acc,
+        [authorName]: acc[authorName] ? 
+        {
+          ...acc[authorName],
+          count: acc[authorName].count + 1
+        } :
+        {
           count: 1,
           name : authorName,
           email : elem.commit.author.email
         }
-      } else if (obj[authorName].name === authorName){
-        obj[authorName].count += 1;
       }
-    })
+    }, {})
+
     const devArray = Object.values(obj);
     const maxCommit = Math.max(...devArray.map(elem => elem.count));
     const bestDevelopers = devArray.filter(elem => elem.count === maxCommit);
+    
     return bestDevelopers;
   })
 
