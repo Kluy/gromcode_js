c01366cb7d96117aba5523fa43746b3ef5f339f6
Kluy <kluy@i.ua>
2022-10-06 15:29:42 +0300

    l24t4c20

--- lesson24/t4/index.js
+++ lesson24/t4/index.js
@@ -2,8 +2,9 @@ const formatter = new Intl.DateTimeFormat("en-US", {
   month: "short",
 });
 
-export const studentsBirthDays = (students) => {
-  const sortedStudents = students  
+// export 
+const studentsBirthDays = (students) => 
+  students  
     .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
     .reduce((acc, elem) => {
       const month = formatter.format(new Date(elem.birthDate));
@@ -12,50 +13,15 @@ export const studentsBirthDays = (students) => {
         [month]: acc[month] ? acc[month].concat(elem.name) : [elem.name]
       }
     }, {})
-    
-    // .map(elem => {
-    //     const month = formatter.format(new Date(elem.birthDate));
-    //   if(sortedBirthDays[month] === undefined){
-    //     sortedBirthDays[month] = [];
-    //   }
-    //   sortedBirthDays[month].push(elem.name); 
-    // });
 
-    return sortedStudents;
-  }
-
-// export 
-// const studentsBirthDays = students => {
-//   const bdayObj = students.reduce((acc, student) => {
-//     const month = formatter.format(new Date(student.birthDate));
-//     console.log(month);
-
-//     return {
-//       ...acc,
-//       [month]: acc[month] ? acc[month].concat(student) : [student],
-//     };
-//   }, {});
-
-//   return Object.entries(bdayObj).reduce(
-//     (acc, [month, studentsList]) => ({
-//       ...acc,
-//       [month]: studentsList
-//       .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
-//       .map(({name}) => name),
-//     }),
-//     {},
-//   );
-// }
-
-  
-  // console.log(studentsBirthDays(
-  //   [
-  //     {name: 'Tom', birthDate:'01/17/2008'},
-  //     {name: 'Tom4', birthDate:'01/17/2007'},
-  //     {name: 'Tom2', birthDate:'01/29/2000'},
-  //     {name: 'Tom5', birthDate:'01/30/1999'},
-  //     {name: 'Tom3', birthDate:'04/05/2011'},
-  //     {name: 'Tom6', birthDate:'02/19/2009'},
-  //     {name: 'Tom1', birthDate:'02/20/2009'},
-  //   ]
-  // ));
+  console.log(studentsBirthDays(
+    [
+      {name: 'Tom', birthDate:'01/17/2008'},
+      {name: 'Tom4', birthDate:'01/17/2007'},
+      {name: 'Tom2', birthDate:'01/29/2000'},
+      {name: 'Tom5', birthDate:'01/30/1999'},
+      {name: 'Tom3', birthDate:'04/05/2011'},
+      {name: 'Tom6', birthDate:'02/19/2009'},
+      {name: 'Tom1', birthDate:'02/20/2009'},
+    ]
+  ));
