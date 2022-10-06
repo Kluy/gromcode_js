6c143dce0051f584a93154718f10bdccb1533e7a
Kluy <kluy@i.ua>
2022-10-06 12:40:24 +0300

    l24t4c16

--- lesson24/t4/index.js
+++ lesson24/t4/index.js
@@ -4,7 +4,7 @@ export const studentsBirthDays = (students) => {
       elem.birthDate = new Date(elem.birthDate);
       return elem;
     }).sort((a, b) => {
-      return a.birthDate > b.birthDate;
+      return a.birthDate.getDate() > b.birthDate.getDate();
     }).map(elem => {
       const month = elem.birthDate.getMonth();
       if(sortedBirthDays[month] === undefined){
@@ -12,5 +12,43 @@ export const studentsBirthDays = (students) => {
       }
       sortedBirthDays[month].push(elem.name); 
     });
+
     return sortedBirthDays;
   }
+
+// export
+//  const studentsBirthDays = students => {
+//   const bdayObj = students.reduce((acc, student) => {
+//     const month = new Date(student.birthDate).getMonth();
+//     console.log(acc);
+
+//     return {
+//       ...acc,
+//       [month]: acc[month] ? acc[month].concat(student) : [student],
+//     };
+//   }, {});
+//   console.log(bdayObj);
+
+//   return Object.entries(bdayObj).reduce(
+//     (acc, [month, studentsList]) => ({
+//       ...acc,
+//       [month]: studentsList
+//       .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
+//       .map(({name}) => name),
+//     }),
+//     {},
+//   );
+// }
+
+  
+  // console.log(studentsBirthDays(
+  //   [
+  //     {name: 'Tom', birthDate:'01/17/2008'},
+  //     {name: 'Tom4', birthDate:'01/17/2007'},
+  //     {name: 'Tom2', birthDate:'01/29/2000'},
+  //     {name: 'Tom5', birthDate:'01/30/1999'},
+  //     {name: 'Tom3', birthDate:'04/05/2011'},
+  //     {name: 'Tom6', birthDate:'02/19/2009'},
+  //     {name: 'Tom1', birthDate:'02/20/2009'},
+  //   ]
+  // ));
