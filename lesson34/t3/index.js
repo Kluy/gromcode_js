6a33b594a2c5fbb49af0c042c19c903568887e39
Kluy <kluy@i.ua>
2022-10-07 22:51:28 +0300

    l34t3c2

--- lesson34/t3/index.js
+++ lesson34/t3/index.js
@@ -1,16 +1,28 @@
-const avatarElem = document.querySelector('.user__avatar');
-const nameElem = document.querySelector('.user__name');
-const locationElem = document.querySelector('.user__location');
-const buttonInput = document.querySelector('.name-form__btn');
-const inputValue = document.querySelector('.name-form__input');
+const formElem = document.querySelector('.login-form');
+const submitButtonElem = document.querySelector('.submit-button');
 
-const sendRquest = () => fetch(`https://api.github.com/users/${inputValue.value}`).then(response => response.json()).then(result => renderData(result));
 
-buttonInput.addEventListener('click', sendRquest);
-
-const renderData = (result) => {
-  const {avatar_url, name, location} = result;
-  avatarElem.src = avatar_url;
-  nameElem.textContent = name;
-  locationElem.textContent = location ? `from ${location}` : '';
+const submitForm = (event) => {
+  event.preventDefault();
+  const data = Object.fromEntries(new FormData(formElem));
+  console.log(formElem.reportValidity());
+  if(formElem.reportValidity())
+submitButtonElem.removeAttribute('disabled');
+  console.log(data);
+  console.log(report);
 }
+
+
+
+submitButtonElem.addEventListener('click', submitForm);
+
+// const sendRquest = () => fetch(`https://api.github.com/users/${inputValue.value}`).then(response => response.json()).then(result => renderData(result));
+
+// buttonInput.addEventListener('click', sendRquest);
+
+// const renderData = (result) => {
+//   const {avatar_url, name, location} = result;
+//   avatarElem.src = avatar_url;
+//   nameElem.textContent = name;
+//   locationElem.textContent = location ? `from ${location}` : '';
+// }
