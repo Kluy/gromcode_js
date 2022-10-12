243a98fac5511b9f1eb8a6db091d4497639950d0
Kluy <kluy@i.ua>
2022-10-12 12:21:06 +0300

    l33t3c8

--- lesson34/t3/index.js
+++ lesson34/t3/index.js
@@ -16,7 +16,7 @@ const submitForm = (event) => {
       'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(data),
-  }).then(response => response.json()).then(result =>  alert(Object.entries(result))).then(() => formElem.reset());
+  }).then(response => response.json()).then(result => alert(JSON.stringify(result))).then(() => formElem.reset());
 }
 
 formElem.addEventListener('input', validateForm);
