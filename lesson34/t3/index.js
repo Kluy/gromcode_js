bfea084131b817fcba97811687ed1fb299204b87
Kluy <kluy@i.ua>
2022-10-12 11:25:20 +0300

    l34t3c2

--- lesson34/t3/index.js
+++ lesson34/t3/index.js
@@ -1,28 +1,25 @@
 const formElem = document.querySelector('.login-form');
 const submitButtonElem = document.querySelector('.submit-button');
 
+const validateForm = () => {
+  if(formElem.reportValidity())
+  submitButtonElem.removeAttribute('disabled');
+}
 
 const submitForm = (event) => {
   event.preventDefault();
   const data = Object.fromEntries(new FormData(formElem));
-  console.log(formElem.reportValidity());
-  if(formElem.reportValidity())
-submitButtonElem.removeAttribute('disabled');
   console.log(data);
-  console.log(report);
+
+  fetch('https://633ecd660dbc3309f3bda154.mockapi.io/api/v1/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(data),
+  })
+  formElem.reset();
 }
 
-
-
-submitButtonElem.addEventListener('click', submitForm);
-
-// const sendRquest = () => fetch(`https://api.github.com/users/${inputValue.value}`).then(response => response.json()).then(result => renderData(result));
-
-// buttonInput.addEventListener('click', sendRquest);
-
-// const renderData = (result) => {
-//   const {avatar_url, name, location} = result;
-//   avatarElem.src = avatar_url;
-//   nameElem.textContent = name;
-//   locationElem.textContent = location ? `from ${location}` : '';
-// }
+formElem.addEventListener('input', validateForm);
+formElem.addEventListener('submit', submitForm);
