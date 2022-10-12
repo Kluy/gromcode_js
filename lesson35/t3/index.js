09fecb21e79f9535ab1ee1ab1c7bc8130a44ded4
Kluy <kluy@i.ua>
2022-10-12 16:44:07 +0300

    l35t3c2

--- lesson35/t3/index.js
+++ lesson35/t3/index.js
@@ -1,5 +1,7 @@
 import { getData } from "./scripts/getData.js";
-
+const avatarElem = document.querySelector('.user__avatar');
 const buttonElem = document.querySelector('.name-form__btn');
+
+avatarElem.src ="https://avatars3.githubusercontent.com/u10001";
 
 buttonElem.addEventListener('click', getData);
--- lesson35/t3/scripts/appendList.js
+++ lesson35/t3/scripts/appendList.js
@@ -3,9 +3,10 @@ const listElem = document.querySelector('.repo-list');
 export const createListElem = (arr) => {
   const listItemElem = document.createElement('li');
   listItemElem.classList.add('repo-list__item')
-  arr.map(elem => {
+  const reposList = arr.map(elem => {
     listItemElem.textContent = elem.name;
     elem = listItemElem.outerHTML;
-    listElem.innerHTML += elem;
-  });
+    return elem;
+  }).join('');
+  listElem.innerHTML = reposList;
 }
