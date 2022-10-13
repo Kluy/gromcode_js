1461d711e5a7144acade338c3eff939cb1c5d627
Kluy <kluy@i.ua>
2022-10-13 14:02:09 +0300

    l36t2c3

--- lesson36/t2/scripts/gateway.js
+++ lesson36/t2/scripts/gateway.js
@@ -4,10 +4,3 @@ export const sendRequest = async (url) => {
     return await response.json();
     throw new Error('Failed to load data');
 }
-
-export const sendRepoRequest = async (url) => { 
-    const response = await fetch(`${url}`);
-    if(response.ok)
-    return await response.json();
-    throw new Error('Failed to load data');
-}
