a7b3d36a1f643932a515e7757ee2a4a46f7a4ef9
Kluy <kluy@i.ua>
2022-10-13 15:36:39 +0300

    l36t3c2

--- lesson36/t3/index.js
+++ lesson36/t3/index.js
@@ -7,13 +7,17 @@
 
 export const getUsersBlogs = async users => {
   // put your code here
-    const result = await Promise.all(users.map(elem => fetch(`https://api.github.com/users/${elem}`)));
-    if(result.ok){
-      return result.map(elem => elem.json().then(result => result.blog));
-    }
+  const result = await Promise.all(users.map(elem => fetch(`https://api.github.com/users/${elem}`)
+  .then(response => {
+    if(response.ok)
+    return response.json()
     throw new Error('Error');
+  })
+    .then(result => result.blog)));
+
+  return result;
 };
 
 // examples
 // getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)).catch(e => console.log(e)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)).catch(e => console.log(e)); // ==> ["https://opensource.microsoft.com"]
