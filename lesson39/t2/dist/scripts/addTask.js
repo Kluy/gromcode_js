e15d3cf9fa054a8373641589c5bb94e9b6129049
Kluy <kluy@i.ua>
2022-10-20 14:36:23 +0300

    l39t2c2

--- lesson39/t2/dist/scripts/addTask.js
+++ lesson39/t2/dist/scripts/addTask.js
@@ -1,14 +1,16 @@
 import { renderTasks } from "./render.js";
 import { addTask } from "./gateway.js";
-export const addNewTask = () => {
-  const taskInput = document.querySelector('.task-input');
-  const taskText = taskInput.value;
+export var addNewTask = function addNewTask() {
+  var taskInput = document.querySelector('.task-input');
+  var taskText = taskInput.value;
   if (taskText !== '') {
     addTask({
       task: taskText,
       done: false,
       date: new Date()
-    }).then(() => renderTasks());
+    }).then(function () {
+      return renderTasks();
+    });
     taskInput.value = '';
   }
 };
