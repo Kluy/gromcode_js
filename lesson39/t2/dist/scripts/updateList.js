ab1f8b61492f57265d81f9bdc65a110364e9c110
Kluy <kluy@i.ua>
2022-10-20 16:10:30 +0300

    l39t2c3

--- lesson39/t2/dist/scripts/updateList.js
+++ lesson39/t2/dist/scripts/updateList.js
@@ -1,23 +1,13 @@
-import "core-js/modules/es.array.find.js";
-import "core-js/modules/es.object.to-string.js";
 import { renderTasks } from "./render.js";
 import { deleteTask, getTasks, updateTask } from "./gateway.js";
-export var updateList = function updateList(event) {
-  var taskId = event.target.parentElement.dataset.id || event.target.dataset.id;
+export const updateList = event => {
+  const taskId = event.target.parentElement.dataset.id || event.target.dataset.id;
   if (event.target.classList.contains('list__item-delete-button')) {
-    deleteTask(taskId).then(function () {
-      return renderTasks();
-    });
+    deleteTask(taskId).then(() => renderTasks());
   } else {
-    getTasks().then(function (result) {
-      return result.find(function (elem) {
-        return elem.id === taskId;
-      });
-    }).then(function (result) {
+    getTasks().then(result => result.find(elem => elem.id === taskId)).then(result => {
       result.done = !result.done;
-      updateTask(result.id, result).then(function () {
-        return renderTasks();
-      });
+      updateTask(result.id, result).then(() => renderTasks());
     });
   }
 };
