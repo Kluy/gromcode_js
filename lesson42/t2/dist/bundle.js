66d4223f0a50144dd0fd2682bdf117cf22558765
Kluy <kluy@i.ua>
2022-10-22 17:12:54 +0300

    l42t2c1

---
+++ lesson42/t2/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(o=>{const{name:e,company:n}={name:"Tom",age:17,company:"Gromcode"};console.log(`${e} from ${n}`)})();const o=[1,2,[3,4,[5,6]]].flat(2);console.log(o),Promise.resolve().finally()})();
