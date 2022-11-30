5f41e53353eb1e782dfc04473ac835a09251a8d0
Kluy <kluy@i.ua>
2022-11-30 22:59:16 +0200

    l55t2c1

--- lesson55-react/t2/src/ProductsList.jsx
+++ lesson55-react/t2/src/ProductsList.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const ProductsList = (props) => {
+  const items = props.cartItems
+
+  const summ = items.reduce((acc, {price}) => acc + price, 0)
+
   return (
     <div className="products">
       <ul className="products__list">
-        <li className="products__list-item">
-          <span className="products__item-name">iPhone 11</span>
-          <span className="products__item-price">$999</span>
+        {items.map(({id, name, price}) =>
+        <li key={id} className="products__list-item">
+          <span className="products__item-name">{name}</span>
+          <span className="products__item-price">{price}</span>
         </li>
-        <li className="products__list-item">
-          <span className="products__item-name">iPad Pro</span>
-          <span className="products__item-price">$799</span>
-        </li>
+        )}
       </ul>
-      <div className="products__total">Total: $1798</div>
+      <div className="products__total">Total: ${summ}</div>
     </div>
   );
 };
