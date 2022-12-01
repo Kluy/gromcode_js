66e838d2b42feb9cf96040e82213b45bb09c9f47
Kluy <kluy@i.ua>
2022-12-01 21:35:36 +0200

    l55t3c1

--- lesson55-react/t3/src/Calculator.jsx
+++ lesson55-react/t3/src/Calculator.jsx
@@ -3,11 +3,45 @@ import BoilingVerdict from './BoilingVerdict';
 import TemperatureInput from './TemperatureInput';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: '', scale: 'c' };
+  }
+
+  handleCelsiusChange = (temperature) => {
+    this.setState({ scale:'c', temperature });
+  }
+
+  handleFahrenheitChange = (temperature) => {
+    this.setState({ scale:'f', temperature });
+  }
+
+  toCelsius = (fahrenheit) => ((fahrenheit - 32) * 5 / 9);
+
+  toFahrenheit = (celsius) => ((celsius * 9 / 5) + 32);
+
+  tryConvert = (temperature, convert) => {
+    const input = parseFloat(temperature);
+    if(Number.isNaN(input)){
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+
   render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'f' ? tryConvert(temperature, toFahrenheit) : temperature;
     return (
       <div>
-        <TemperatureInput scale="c" />
-        <TemperatureInput scale="f" />
+        <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange}/>
+        <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange}/>
+        <BoilingVerdict celsius={parseFloat(celsius)} />
       </div>
     );
   }
--- lesson55-react/t3/src/TemperatureInput.jsx
+++ lesson55-react/t3/src/TemperatureInput.jsx
@@ -11,12 +11,12 @@ class TemperatureInput extends React.Component {
     this.state = { temperature: '' };
   }
 
-  handleChange = () => {
-    this.detState({ temperature: e.target.value });
+  handleChange = (e) => {
+    this.props.onTemperatureChange(e.target.value);
   };
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
