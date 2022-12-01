62bdd658ecc91b65ba57260680d3d5ed65f9e6a7
Kluy <kluy@i.ua>
2022-12-01 22:14:12 +0200

    l55t3c1

--- lesson55-react/t3/src/BoilingVerdict.jsx
+++ lesson55-react/t3/src/BoilingVerdict.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const BoilingVerdict = (props) => {
   if (props.celsius >= 100) {
-    return <p>The water would boil</p>;
+    return <p>The water would boil.</p>;
   }
-  return <p>The water would not boil</p>;
+  return <p>The water would not boil.</p>;
 };
 
 export default BoilingVerdict;
