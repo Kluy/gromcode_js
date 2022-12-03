523c29c8eedc23a153546ba3293ff57dab3deaa3
Kluy <kluy@i.ua>
2022-12-03 18:59:00 +0200

    l56t1c1

--- lesson56-react/t2/src/App.jsx
+++ lesson56-react/t2/src/App.jsx
@@ -4,11 +4,15 @@ import Dialog from './Dialog';
 class App extends Component {
   state = {isOpen: false}
 
+  handleClose = () => {
+    this.setState({isOpen:false});
+  }
+
   render() {
     return (
-      <div class="app">
-        <button class="btn">Show dialog</button>
-        <Dialog title='Recommendation'>
+      <div className="app">
+        <button onClick={() => this.setState({isOpen:true})} className="btn">Show dialog</button>
+        <Dialog onClose={this.handleClose} isOpen={this.state.isOpen} title='Recommendation'>
           <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
         </Dialog>
       </div>
--- lesson56-react/t2/src/Dialog.jsx
+++ lesson56-react/t2/src/Dialog.jsx
@@ -2,13 +2,15 @@ import React, { PureComponent } from 'react';
 
 class Dialog extends PureComponent {
   render(){
+
+    if(this.props.isOpen)
     return (
-      <div class="dialog">
-        <div class="dialog__heading">
-          <h4 class="dialog__title">{this.props.title}</h4>
-          <button onClick={this.props.om} class="dialog__close-btn">+</button>
+      <div className="dialog">
+        <div className="dialog__heading">
+          <h4 className="dialog__title">{this.props.title}</h4>
+          <button onClick={this.props.onClose} className="dialog__close-btn">+</button>
         </div>
-        <div class="dialog__content">
+        <div className="dialog__content">
           {this.props.children}
         </div>
       </div>
