59833efb066eb525f0355aac83157c623e03f927
Kluy <kluy@i.ua>
2022-12-05 22:50:12 +0200

    l57t1c1

--- lesson57-react/t1/src/Task.jsx
+++ lesson57-react/t1/src/Task.jsx
@@ -5,16 +5,15 @@ class Task extends PureComponent  {
   // ({text, done, id, handleDone})
 
   render(){
-  const liClasses = classNames('list-item', {'list-item_done':this.props.done})
-
-  return (
-    <li  className={liClasses}>
-       <input onClick={() => this.props.onChange(this.props.id)} type="checkbox" className="list-item__checkbox" />
-          {this.props.task}
-       <button onClick={() => this.props.onDelete(this.props.id)} className="list-item__delete-btn"></button>
-    </li>
-  );
-}
+    const listItemClasses = classNames('list-item', {'list-item_done':this.props.done});
+    return (
+      <li  className={listItemClasses}>
+         <input onClick={() => this.props.onChange(this.props.id)} type="checkbox" defaultChecked={this.props.done} className="list-item__checkbox" />
+            {this.props.task}
+         <button onClick={() => this.props.onDelete(this.props.id)} className="list-item__delete-btn"></button>
+      </li>
+    );
+  }
 }
 
 export default Task;
