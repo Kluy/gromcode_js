13ab847df146d2497e535c379957287ea5ff007b
Kluy <kluy@i.ua>
2022-12-09 19:24:11 +0200

    l59t1c1

--- lesson59-react/t1/src/Counter.jsx
+++ lesson59-react/t1/src/Counter.jsx
@@ -3,14 +3,14 @@ import React, {useState} from 'react';
 const Counter = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <div class="counter">
-      <button class="counter__button" onClick={() => setCounter(counter - 1)}>
+    <div className="counter">
+      <button className="counter__button" onClick={() => setCounter(counter - 1)}>
         -
       </button>
-      <span class="counter__value">
+      <span className="counter__value">
         {counter}
       </span>
-      <button class="counter__button" onClick={() => setCounter(counter + 1)}>
+      <button className="counter__button" onClick={() => setCounter(counter + 1)}>
         +
       </button>
     </div>
