451bf218ed3e33d5641e435c3001d161f0779873
Kluy <kluy@i.ua>
2022-12-11 23:34:28 +0200

    l58t3c4

--- lesson58-react/t3/src/User.jsx
+++ lesson58-react/t3/src/User.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { fetchData } from './gateway';
 
 class User extends Component {
   constructor(props){
@@ -11,32 +10,48 @@ class User extends Component {
     }
   }
 
-render (){
-
- fetchData(this.props.match.params.userId)
-    .then(({name, location, avatar_url}) => {
+ fetchData = () => fetch(`https://api.github.com/users/${this.props.match.params.userId}`)
+ .then(response => response.json())
+ .then(({name, location, avatar_url}) => {
+      console.log('fetch')
       this.setState({
         name:name,
         location:location,
-        avatar_url:avatar_url
+        avatar_url:avatar_url,
       });
     }
   )
 
-  return (
-    <div className="user">
-      <img
-        alt="User Avatar"
-        src={this.state.avatar_url}
-        className="user__avatar"
-        />
-      <div className="user__info">
-        <span className="user__name">{this.state.name}</span>
-        <span className="user__location">{this.state.location}</span>
+  componentDidMount(){
+    console.log('didmount')
+    this.fetchData();
+  }
+  
+  componentDidUpdate(prevProps){
+    console.log('didupdate')
+
+    if(prevProps.match.params.userId !== this.props.match.params.userId){
+
+      this.fetchData();
+    }
+  }
+
+  render (){
+    console.log('render');
+    return (
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src={this.state.avatar_url}
+          className="user__avatar"
+          />
+        <div className="user__info">
+          <span className="user__name">{this.state.name}</span>
+          <span className="user__location">{this.state.location}</span>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 };
 
 export default User;
--- lesson58-react/t3/src/gateway.js
+++
@@ -1,4 +0,0 @@
-
-export const fetchData = (userId) =>
-  fetch(`https://api.github.com/users/${userId}`)
-    .then(response => response.json());
