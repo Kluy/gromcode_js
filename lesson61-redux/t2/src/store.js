f606e34f46b9594ae9ffe6ba04886d311854c374
Kluy <kluy@i.ua>
2023-01-06 12:49:29 +0200

    l61t2c1

--- lesson61-redux/t2/src/store.js
+++ lesson61-redux/t2/src/store.js
@@ -1,29 +1,38 @@
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
 
 export const increment = () => {
-    return {
-        type: INCREMENT,
-    }
-}
+  return {
+    type: INCREMENT,
+  };
+};
 
 export const decrement = () => {
-    return {
-        type: DECREMENT,
-    }
-}
+  return {
+    type: DECREMENT,
+  };
+};
+
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
 
 const counterReducer = (state = 0, action) => {
-    switch(action.type){
-        case INCREMENT:
-            return state + 1;
-        case DECREMENT:
-            return state - 1;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    case RESET:
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 export const store = createStore(counterReducer);
