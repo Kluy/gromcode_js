95ecf230de7119d47578d95637ffaf19de2647a5
Kluy <kluy@i.ua>
2023-01-10 21:15:49 +0200

    l61t3c12

--- lesson61-redux/t3/src/users.actions.js
+++ lesson61-redux/t3/src/users.actions.js
@@ -1,14 +1,14 @@
-import { ADD_USER, DELETE_USER } from "./users.reducer";
+// import { ADD_USER, DELETE_USER } from "./users.reducer";
 
 export const addUser = () => {
   return {
-    type: ADD_USER,
+    type: 'ADD_USER',
   };
 };
 
 export const deleteUser = (id) => {
   return {
-    type: DELETE_USER,
+    type: 'DELETE_USER',
     id,
   };
 };
--- lesson61-redux/t3/src/users.reducer.js
+++ lesson61-redux/t3/src/users.reducer.js
@@ -1,5 +1,5 @@
-export const ADD_USER = 'ADD_USER';
-export const DELETE_USER = 'DELETE_USER';
+// export const ADD_USER = 'ADD_USER';
+// export const DELETE_USER = 'DELETE_USER';
 
 const initialState = {
   usersList: []
@@ -7,12 +7,12 @@ const initialState = {
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_USER:
+    case 'ADD_USER':
       return {
         ...state,
         usersList: state.usersList.concat({id: 76, name: 'Tom'})
       };
-    case DELETE_USER:
+    case 'DELETE_USER':
       return {
         ...state,
         usersList: state.usersList.filter(elem => elem.id !== action.id)
