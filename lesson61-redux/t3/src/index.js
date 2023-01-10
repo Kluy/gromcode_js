8feeb51b2ec30cda4c0114b554321e0edff25ab1
Kluy <kluy@i.ua>
2023-01-10 21:32:31 +0200

    l61t3c15

--- lesson61-redux/t3/src/index.js
+++ lesson61-redux/t3/src/index.js
@@ -1,7 +1,21 @@
 import store from "./store.js";
 import { addUser, deleteUser } from "./users.actions.js";
 
+import './index.scss';
 
+const addUserBtn = document.querySelector('[data-action="increment"]');
+const deleteUserBtn = document.querySelector('[data-action="decrement"]');
+
+const handleAddUser = () =>{
+    store.dispatch(addUser({id: 76, name: 'Tom'}))
+}
+
+const handleDeleteUser = () =>{
+    store.dispatch(deleteUser(76))
+}
+
+addUserBtn.addEventListener('click', handleAddUser);
+deleteUserBtn.addEventListener('click', handleDeleteUser);
 
 store.dispatch(addUser({id: 76, name: 'Tom'}));
 store.dispatch(deleteUser(76));
