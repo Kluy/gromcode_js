9e56abca19fc216ff46993bf7927aa6840da07f8
Kluy <kluy@i.ua>
2023-01-12 23:47:11 +0200

    l62t3c4

--- lesson62-redux/t3/src/index.js
+++ lesson62-redux/t3/src/index.js
@@ -1,7 +1,13 @@
 import { addProduct, removeProduct } from "./cart.actions.js";
+import { setLanguage } from "./language.actions.js";
 import store from "./store.js";
+import { removeUser, setUser } from "./user.actions.js";
 
 store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(addProduct({id:1,name:'ddd'}));
+store.dispatch(addProduct({id:2,name:'fff'}));
 store.dispatch(removeProduct(1));
+store.dispatch(setLanguage('es'));
+store.dispatch(setUser({name:'Bob'}));
+store.dispatch(removeUser());
--- lesson62-redux/t3/src/language.reducer.js
+++ lesson62-redux/t3/src/language.reducer.js
@@ -3,6 +3,8 @@ import { SET_LANGUAGE } from "./language.actions";
 export const languageReducer = (state = 'en', action) => {
     switch (action.type){
         case SET_LANGUAGE:
-            return action.payload.language
+            return action.payload.language;
+        default:
+            return state;
     }
 }
