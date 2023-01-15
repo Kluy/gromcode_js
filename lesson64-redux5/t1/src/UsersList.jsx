1f3cd8b0b9eaed8e72ef0c32646723ced69672bc
Kluy <kluy@i.ua>
2023-01-15 13:00:58 +0200

    l64t1c6

--- lesson64-redux5/t1/src/UsersList.jsx
+++ lesson64-redux5/t1/src/UsersList.jsx
@@ -5,8 +5,12 @@ import Pagination from './Pagination';
 import User from './User';
 import { currentPageSelector, usersListSelector } from './users.selectors';
 
+const itemsPerPage = 3;
 
 const UsersList = ({usersList, currentPage, goNext, goPrev}) => {
+
+  const firstUser = currentPage * itemsPerPage;
+
   return (
       <div>
         <Pagination
@@ -14,12 +18,12 @@ const UsersList = ({usersList, currentPage, goNext, goPrev}) => {
           goNext={goNext}
           currentPage={currentPage}
           totalItems={usersList.length}
-          itemsPerPage={3}
+          itemsPerPage={itemsPerPage}
         />
         <ul className="users">
           {usersList
             .sort((a, b) => a.name > b.name ? 1 : -1)
-            .slice(currentPage * 3, (currentPage * 3) + 3)
+            .slice(firstUser, firstUser + 3)
             .map((user) => (
               <User key={user.id} {...user} />
             ))}
